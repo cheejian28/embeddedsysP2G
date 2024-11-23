d890ab2f06aa6e6ee5ef86790bba5ca477170c06
txh <2303404@sit.singaporetech.edu.sg>
2024-11-23 17:34:14 +0800

    Added speed change - no line following

---
+++ RTOS_xh/irline_following.c
@@ -0,0 +1,50 @@
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include <stdio.h>
+
+#define IR_PIN 28 // GPIO pin where the IR module is connected
+
+extern volatile uint32_t line_input = 0;
+
+void setUpLineFollowing()
+{
+    adc_init();
+    adc_gpio_init(IR_PIN);
+    adc_select_input(2);
+}
+
+void getValue()
+{
+    line_input = adc_read();
+}
+
+// // int main()
+// {
+//     // Initialize the GPIO pin for the IR sensor
+//     stdio_init_all();
+//     adc_init();
+//     adc_gpio_init(IR_PIN);
+//     adc_select_input(2);
+//     // gpio_set_dir(IR_PIN, GPIO_IN);
+
+//     while (true)
+//     {
+//         // Read the value from the IR sensor
+//         uint32_t lineDetected = adc_read();
+
+//         // printf("%d \n", lineDetected);
+
+//         // if (lineDetected > 1000)
+//         // {
+//         //     printf("it's black \n");
+//         // }
+//         // else
+//         // {
+//         //     printf("it's white \n");
+//         // }
+
+//         // Add a small delay for stability
+//         sleep_ms(100);
+//     }
+//     return 0;
+// }
